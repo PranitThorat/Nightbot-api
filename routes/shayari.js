827c39df4082fa5f00e559a9654fffe323651985
Pranit Thorat <159506760+PranitThorat@users.noreply.github.com>
2026-07-01 09:52:51 +0530

    Update shayari.js

--- routes/shayari.js
+++ routes/shayari.js
@@ -1,1 +1,16 @@
+import shayari from "../data/shayari.json";
 
+export async function getShayari() {
+
+    const random =
+        shayari.shayari[
+            Math.floor(Math.random() * shayari.shayari.length)
+        ];
+
+    return new Response(random, {
+        headers: {
+            "Content-Type": "text/plain;charset=UTF-8"
+        }
+    });
+
+}
