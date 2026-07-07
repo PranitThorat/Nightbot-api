cac55d5e7bdfcb2f8cbb47a97b7c5d9bb8b5a90f
Pranit Thorat <159506760+PranitThorat@users.noreply.github.com>
2026-07-07 19:02:21 +0530

    Create roastData.js

---
+++ data/roastData.js
@@ -0,0 +1,33 @@
+export const roastData = {
+
+    amora: {
+
+        aliases: [
+
+            "@cozy_panda_gaming",
+            "cozy_panda_gaming",
+            "@purplepandaplayz",
+            "purplepandaplayz"
+
+        ],
+
+        title: "💜 ROAST SYSTEM ERROR",
+
+        message:
+            "Roast.exe crashed... Stream Queen detected. Chat unanimously refused to roast today. 💜"
+
+    },
+
+    normalRoasts: [
+
+
+
+    ],
+
+    reverseRoasts: [
+
+
+
+    ]
+
+};
